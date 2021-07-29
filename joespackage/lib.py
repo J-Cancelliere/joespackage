680613f1ec1593d7d8b52d75a1812bd5a33af085
Joe Cancelliere <cancellierejm@gmail.com>
2021-07-29 19:41:27 +0100

    Initial Push

---
+++ joespackage/lib.py
@@ -0,0 +1,7 @@
+def try_me():
+    answer = input('What is the answer to life, the universe, and everything?\n')
+    if answer == '42':
+        reply = 'Correct!'  
+    else:
+        reply = 'Incorrect!'
+    return reply
