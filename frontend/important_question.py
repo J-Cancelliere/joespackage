5cd33941b1329154f01794cb7aacaf5d9b86a3aa
Joe Cancelliere <cancellierejm@gmail.com>
2021-09-23 19:09:06 +0100

    Add comments for readability

--- frontend/important_question.py
+++ frontend/important_question.py
@@ -2,20 +2,21 @@ import streamlit as st
 from joespackage.lib import try_me
 from PIL import Image
 
+#Display image of Deepthought
 image = Image.open("deep_thought.jpg")
-
 st.image(image,)
 
+# Ask the user the question
 st.markdown("""
     <h1 style='text-align: center;'>What is the answer to life, the universe, and everything?</h1>
     """, unsafe_allow_html = True)
 
-buff, user_input, buff2 = st.columns([1,1,1])
+# Get the user's answer
+buff, col, buff2 = st.columns([1,1,1])
+user_input = col.text_input("","")
 
-user_input = user_input.text_input("","")
-
+# Show whether the answer is correct or incorrect
 correct_answers = ["42","forty two","forty-two"] 
-
 if user_input.lower().strip() in correct_answers:
     st.markdown("""
     <h1 style='text-align: center; color: green;'>Correct!</h1>
