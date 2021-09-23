a9c3dfe42d0b373afd661618f067b35cfa5d041f
Joe Cancelliere <cancellierejm@gmail.com>
2021-09-23 13:49:52 +0100

    add deep thought image

--- frontend/important_question.py
+++ frontend/important_question.py
@@ -1,5 +1,10 @@
 import streamlit as st
 from joespackage.lib import try_me
+from PIL import Image
+
+image = Image.open("deep_thought.jpg")
+
+st.image(image)
 
 user_input = st.text_input("What is the answer to life, the universe, and everything?", "")
 
